84349d3b914b59c9cef13f75e5ebd85b1f242d8c
EvilArthas <doggy220500@gmail.com>
2024-04-07 13:26:01 +0300

    commit

--- module_8_7_5-2.js
+++ module_8_7_5-2.js
@@ -2,9 +2,27 @@
 // новый массив, содержащий только уникальные числа, отсортированные в порядке возрастания.
 // filterArrayWithReturningOnlyUniqueValuesAndValuesInAscendingOrder
 const arrForTest = [1,1,3,3,5,6,16,2,91,56,88,87,56]
-function filterArray (arr){
-  let uniqueSet = new Set(arr)
-  let uniqueArr = Array.from(uniqueSet)
-  return uniqueArr.sort((a,b) => a-b)
+function filterArray(arr){
+  let uniqueArray = []
+  for (i=0; i<arr.length;i++){
+    if (!uniqueArray.includes(arr[i])){
+      uniqueArray.push(arr[i])
+    }
+  }
+  // [1, 3, 5, 6, 16, 2, 91, 56, 88, 87]
+  let ascendArr = []
+  for(i=0;i<uniqueArray.length;i++){
+    let maxValueOfRestArray = uniqueArray.reduce((accumulator,currentValue)=>{
+      if (accumulator>currentValue){
+        return accumulator
+      } else {
+        return currentValue
+      }
+    })
+    ascendArr[(uniqueArray.length-1-i)] = maxValueOfRestArray
+    let index = uniqueArray.indexOf(maxValueOfRestArray)
+    delete uniqueArray[index]
+  }
+  return ascendArr
 }
 console.log(filterArray(arrForTest))
